cf8ab80459273978d25545c964547cdf04393c4b
Kauê Padilha Alves <kaue181102@gmail.com>
2022-11-07 08:15:06 -0300

    atualização dos jogos

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createGame(player1, hour, player2){
 
 let delay = -0.4;
 function createCard(date, day, games) {
-  delay = delay + 0.4;
+  delay = delay + 0.3;
   return `
     <div class="card" style="animation-delay: ${delay}s">
     <h2>${date}<span>${day}</span></h2>
@@ -22,20 +22,50 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML = 
+createCard(
+  "23/11",
+  "quarta-feira",
+  createGame("belgium", "16:00", "canada")
+  ) +
+createCard(
+  "24/11",
+  "quinta-feira",
+  createGame("brazil", "16:00", "serbia")
+  ) +
+createCard(
+  "25/11",
+  "sexta-feira",
+  createGame("england", "16:00", "unitedStates")
+  ) +
+createCard(
+  "26/11",
+  "sábado",
+  createGame("france", "13:00", "denmark")
+  ) +
   createCard(
-    "24/11",
-    "quinta-feira",
-    createGame("brazil", "16:00", "serbia")
+    "27/11",
+    "Domingo",
+    createGame("germany", "16:00", "spain")
   ) +
   createCard(
     "28/11",
     "segunda-feira",
     createGame("brazil", "13:00", "switzerland") +
-    createGame("portugal", "13:00", "uruguay")
+    createGame("portugal", "16:00", "uruguay")
   ) +
   createCard(
-    "02/11",
+    "30/11",
+    "quarta-feira",
+    createGame("argentina", "16:00", "poland")
+  ) +
+  createCard(
+    "01/12",
+    "quinta-feira",
+    createGame("belgium", "12:00", "croatia")
+  ) +
+  createCard(
+    "02/12",
     "sexta-feira",
-    createGame("brazil", "16:00", "cameroon")
-  )
+    createGame("brazil", "16:00", "cameroon") 
+  ) 
 
